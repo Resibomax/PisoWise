c3e01ba40f75364321c25caf2a4c88e330658c84
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-06-19 19:00:56 +0800

    feat: added formatting

--- Apps/pisowise-fe/app/landing.tsx
+++ Apps/pisowise-fe/app/landing.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Landing() {
   return (
     <div
-      className="flex flex-col items-center justify-center h-screen font-[Ember] text-4xl"
+      className="flex flex-col items-center justify-center h-screen font-[Ember]"
       style={{
         backgroundImage: "url('/assets/bg-lines.svg')",
         backgroundRepeat: "repeat",
@@ -19,11 +19,20 @@ export default function Landing() {
         <br />
         Smarter.
       </div>
-      <div>
-        <Button className="mt-4 text-[#123524]" variant="outline" size="lg">
+      <div className="text-center mt-4">
+        <Button
+          className="mt-4 text-[#123524] text-[16px]"
+          variant="outline"
+          size="lg"
+        >
           Create Account
         </Button>
-        <Button className="mt-4 text-[#123524]" variant="default" size="lg">
+        <br />
+        <Button
+          className="mt-4 text-[#FBF5F3] text-[16px]"
+          variant="soft"
+          size="lg"
+        >
           Sign in
         </Button>
       </div>
