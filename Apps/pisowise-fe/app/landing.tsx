ae7be608a96b81eb74cf716653f6c1e62fcadff0
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-06-19 18:48:37 +0800

    feat: added basic elements

--- Apps/pisowise-fe/app/landing.tsx
+++ Apps/pisowise-fe/app/landing.tsx
@@ -1,18 +1,32 @@
+import { Button } from "@/components/ui/button";
+
 export default function Landing() {
   return (
     <div
-      className="flex items-center justify-center h-screen font-[Ember] text-white text-4xl"
+      className="flex flex-col items-center justify-center h-screen font-[Ember] text-4xl"
       style={{
         backgroundImage: "url('/assets/bg-lines.svg')",
         backgroundRepeat: "repeat",
         backgroundSize: "auto",
       }}
     >
-      {/* Landing Page Navbar/Logo Only */}
-      <div className="flex fixed top-0 items-center justify-center pt-6 z-50 w-full">
-        <div className="text-white text-lg font-medium">Logo</div>
+      <div className="text-center text-[40px] font-bold leading-[40px] tracking-[-0.4px] text-[#FBF5F3]">
+        Snap
+        <br />
+        Receipts.
+        <br />
+        Track
+        <br />
+        Smarter.
       </div>
-      Hello PisoWise
+      <div>
+        <Button className="mt-4 text-[#123524]" variant="outline" size="lg">
+          Create Account
+        </Button>
+        <Button className="mt-4 text-[#123524]" variant="default" size="lg">
+          Sign in
+        </Button>
+      </div>
     </div>
   );
 }
