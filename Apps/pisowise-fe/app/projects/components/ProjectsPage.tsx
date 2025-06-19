2e8bdf18ea03b9422c14544dcc2dfd8587756c4b
maksdeb_g <mxdave02@gmail.com>
2025-06-20 00:30:29 +0800

    style: added additional tailwind classes

--- Apps/pisowise-fe/app/projects/components/ProjectsPage.tsx
+++ Apps/pisowise-fe/app/projects/components/ProjectsPage.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import { Plus, SquarePen } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import ProjectCard from "./cards/ProjectCard"
-import CreateProject from "./cards/CreateProjectCard"
-import { CreateProjectModal } from "./modals/CreateProjectModal"
-import { EditProjectModal } from "./modals/EditprojectModal"
-import { useProjectStore } from "@/app/store/projectStore"
-import NoProjectsCard from "./cards/NoProjectsCard"
+import { Plus, SquarePen } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import ProjectCard from "./cards/ProjectCard";
+import CreateProject from "./cards/CreateProjectCard";
+import { CreateProjectModal } from "./modals/CreateProjectModal";
+import { EditProjectModal } from "./modals/EditprojectModal";
+import { useProjectStore } from "@/app/store/projectStore";
+import NoProjectsCard from "./cards/NoProjectsCard";
 
 export default function ProjectsPage() {
   const {
@@ -21,7 +21,7 @@ export default function ProjectsPage() {
     closeCreateModal,
     openEditModal,
     closeEditModal,
-  } = useProjectStore()
+  } = useProjectStore();
 
   return (
     <div className="w-full max-w-7xl mx-auto mt-4 p-4 md:px-8 lg:px-16">
@@ -29,7 +29,7 @@ export default function ProjectsPage() {
         <p className="text-3xl font-[Ember] text-white">Projects</p>
         <Button
           onClick={openCreateModal}
-          className="bg-[#1B1212] hover:bg-[#FBF5F3] hover:text-black text-white font-[Ember] rounded-[12px] text-[16px]"
+          className="bg-[#1B1212] hover:bg-[#FBF5F3] hover:text-black text-white font-[Ember] rounded-[12px] text-[16px] cursor-pointer"
         >
           <Plus className="w-8 h-8" />
           Create
@@ -53,15 +53,13 @@ export default function ProjectsPage() {
                 spent={project.spent}
                 headerAction={
                   <div className="p-1 hover:bg-white rounded-full transition-color hover:text-black">
-                  <SquarePen
-                    className="w-5 h-5 cursor-pointer"
-                    onClick={() => openEditModal(project)}
-                  />
+                    <SquarePen
+                      className="w-5 h-5 cursor-pointer"
+                      onClick={() => openEditModal(project)}
+                    />
                   </div>
-                  
                 }
                 className="w-full rounded-xl shadow-lg p-6"
-                
               />
             ))}
             <CreateProject onClick={openCreateModal} />
@@ -69,7 +67,11 @@ export default function ProjectsPage() {
         )}
       </div>
 
-      <CreateProjectModal isOpen={isCreateModalOpen} onClose={closeCreateModal} onCreate={addProject} />
+      <CreateProjectModal
+        isOpen={isCreateModalOpen}
+        onClose={closeCreateModal}
+        onCreate={addProject}
+      />
 
       <EditProjectModal
         isOpen={isEditModalOpen}
@@ -78,5 +80,5 @@ export default function ProjectsPage() {
         project={selectedProject}
       />
     </div>
-  )
+  );
 }
