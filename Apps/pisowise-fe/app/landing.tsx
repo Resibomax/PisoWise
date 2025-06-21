fabf65042858d23782fb85a0dee9f3f5cd466626
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-06-21 12:43:05 +0800

    feat: responsiveness

--- Apps/pisowise-fe/app/landing.tsx
+++ Apps/pisowise-fe/app/landing.tsx
@@ -35,18 +35,16 @@ export default function Landing() {
       {/* Main content */}
       <div className="flex flex-col items-center justify-center h-full">
         <div className="text-center text-[40px] font-bold leading-[45px] tracking-[-0.4px] text-[#FBF5F3]">
-          Snap
-          <br />
-          Receipts.
-          <br />
-          Track
-          <br />
-          Smarter.
+          <span className="block md:inline">Snap</span>{" "}
+          <span className="block md:inline">Receipts.</span>
+          <br className="hidden md:block" />
+          <span className="block md:inline">Track</span>{" "}
+          <span className="block md:inline">Smarter.</span>
         </div>
         <div className="text-center mt-2">
           <Button
             onClick={openSignup}
-            className="mt-4 text-[#123524] text-[16px]"
+            className="mt-4 text-[#123524] text-[16px] md:w-[376px]"
             variant="outline"
             size="lg"
           >
@@ -55,7 +53,7 @@ export default function Landing() {
           <br />
           <Button
             onClick={openLogin}
-            className="mt-4 text-[#FBF5F3] text-[16px]"
+            className="mt-4 text-[#FBF5F3] text-[16px] md:w-[376px]"
             variant="soft"
             size="lg"
           >
