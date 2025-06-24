db2eaa5d1edf9c349a477bc43256cff55ff7c7c8
Francis Reid Arranguez <fnarranguez@up.edu.ph>
2025-06-25 00:54:43 +0800

    feat: completed add receipt page

--- Apps/pisowise-fe/app/projects/[id]/add/page.tsx
+++ Apps/pisowise-fe/app/projects/[id]/add/page.tsx
@@ -47,7 +47,7 @@ export default function AddProjectPage() {
           onClick={handleInputBoxClick}
           className="flex flex-col items-center justify-center bg-[#1B1212] h-[294px] w-full rounded-[12px] cursor-pointer hover:bg-[#2a1f1f] transition"
         >
-          <ImageIcon className="text-[#FBF5F3] w-[100px] h-[100px] sm:w-[90px] sm:h-[90px] md:w-[100px] md:h-[100px]" />
+          <ImageIcon className="text-[#FBF5F3] w-[100px] h-[100px]" />
           <p className="text-[#FBF5F3] font-roboto text-base sm:text-lg mt-4 text-center">
             Upload a picture of the receipt
           </p>
