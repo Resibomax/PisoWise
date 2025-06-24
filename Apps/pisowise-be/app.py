b2d3ea22c67c5083f6043d2493df8adfdaf4861c
ThatDot <justinsanveloso@gmail.com>
2025-06-24 09:32:41 +0800

    feat: added route for AI Insights endpoint

--- Apps/pisowise-be/app.py
+++ Apps/pisowise-be/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
-from controllers import user_controller, receipt_controller
+from controllers import user_controller, receipt_controller, insight_controller
 
 app = FastAPI()
 
@@ -15,7 +15,7 @@ app.add_middleware(
 
 app.include_router(user_controller.user_router)
 app.include_router(receipt_controller.receipt_router)
-
+app.include_router(insight_controller.insight_router)
 
 @app.get("/")
 def read_root():
---
+++ Apps/pisowise-be/controllers/insight_controller.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from models.insight import AIInsightCreate, AIInsightResponse
+from usecases.insight_usecase import AIInsightUseCase
+from typing import List
+from db.database import get_db
+
+insight_router = APIRouter()
+
+@insight_router.post("/insights", response_model=AIInsightResponse)
+def create_insight(insight: AIInsightCreate, db: Session = Depends(get_db)):
+    uc = AIInsightUseCase(db)
+    return uc.create_insight_usecase(insight)
+
+@insight_router.get("/insights", response_model=List[AIInsightResponse])
+def get_insights(db: Session = Depends(get_db)):
+    uc = AIInsightUseCase(db)
+    return uc.get_all_insights_usecase()
