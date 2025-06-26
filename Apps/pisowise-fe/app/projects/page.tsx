598994adcb1e4374f45c77687e67fc70a1fd9b46
ThatDot <justinsanveloso@gmail.com>
2025-06-26 09:46:20 +0800

    chore: clean up unused imports

--- Apps/pisowise-fe/app/projects/page.tsx
+++ Apps/pisowise-fe/app/projects/page.tsx
@@ -2,8 +2,6 @@
 
 import ProtectedRoute from "@/components/auth/ProtectedRoute";
 import ProjectsPageComponent from "@/components/projects/ProjectsPage";
-import { useAuthStore } from "@/app/store/authStore";
-import { Button } from "@/components/ui/button";
 
 export default function ProjectsPageContainer() {
   return (
