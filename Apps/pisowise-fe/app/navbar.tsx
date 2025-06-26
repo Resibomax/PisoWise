e761559379631e3596c4b528b85441b8bfd66978
ejminguez <ebminguez@up.edu.ph>
2025-06-27 01:20:11 +0800

    fix: navbar styling

--- Apps/pisowise-fe/app/navbar.tsx
+++ Apps/pisowise-fe/app/navbar.tsx
@@ -14,6 +14,7 @@ import { useAuthStore } from "./store/authStore";
 
 export default function Navbar() {
   const { signOut, user } = useAuthStore();
+
   return (
     <div className="flex justify-center z-50 sticky top-0">
       <div className="flex bg-[#1B1212]/45 backdrop-blur-md shadow-lg w-full min-h-[52px] px-4">
@@ -30,12 +31,11 @@ export default function Navbar() {
           <DropdownMenu>
             <DropdownMenuTrigger>
               <div className="flex items-center gap-2 cursor-pointer">
-                {!user?.attributes?.profile && (
+                {!user?.attributes?.profile ? (
                   <CircleUser className="text-white" size={24} />
-                )}
-                {user?.attributes?.profile && (
+                ) : (
                   <Image
-                    src={user?.attributes?.profile}
+                    src={user.attributes.profile}
                     alt="Profile Picture"
                     width={24}
                     height={24}
@@ -46,11 +46,11 @@ export default function Navbar() {
             </DropdownMenuTrigger>
 
             <DropdownMenuContent
-              className="border-none shadow-lg bg-opacity-90"
+              className="border-none shadow-lg bg-opacity-90 backdrop-blur-md"
               align="end"
             >
               <DropdownMenuItem
-                className="cursor-pointer text-white flex justify-center py-2 px-4 rounded-md transition-all duration-500 hover:brightness-120 cursor-pointer"
+                className="cursor-pointer text-white flex justify-center py-2 px-4 rounded-md transition-all duration-500 hover:brightness-110"
                 style={{
                   background:
                     "linear-gradient(355deg, rgba(105, 105, 105, 1) 0%, rgba(207, 207, 207, 0.55) 50%, rgba(235, 232, 232, 1) 100%)",
@@ -58,7 +58,7 @@ export default function Navbar() {
               >
                 <button
                   onClick={signOut}
-                  className="flex items-center gap-2 justify-center"
+                  className="flex items-center gap-2 justify-center cursor-pointer"
                 >
                   <LogOutIcon className="text-white" size={16} />
                   <p>Log out</p>
