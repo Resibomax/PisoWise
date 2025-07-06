56408c3365de95174972c2a573607ffab047262d
ThatDot <justinsanveloso@gmail.com>
2025-07-07 00:05:08 +0800

    chore: add fallback for optional itemsToRender

--- Apps/pisowise-fe/components/receipts/cards/ItemsCard.tsx
+++ Apps/pisowise-fe/components/receipts/cards/ItemsCard.tsx
@@ -44,7 +44,7 @@ export default function ItemsCard({ receipt }: ItemsCardProps) {
     <Card className="p-6 bg-[#1B1212] text-[#FBF5F3] rounded-[12px] md:h-[480px] h-auto flex flex-col">
       <CardHeader className="flex flex-col items-start mb-4">
         <h2 className="text-lg font-roboto-bld">
-          Items ({itemsToRender.length})
+          Items ({itemsToRender.length ?? 0})
         </h2>
       </CardHeader>
 
